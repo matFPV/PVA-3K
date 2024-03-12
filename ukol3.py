16113445f2554c69886730990161805ee73251ec
matpech <47293035+matFPV@users.noreply.github.com>
2024-03-12 12:22:09 +0100

    Update ukol3.py

--- ukol3.py
+++ ukol3.py
@@ -9,10 +9,3 @@ def next_smallest_palindrome(from_num):
         if is_palindrome(from_num):
             return from_num  # Success
         from_num += 1
-
-# Example usage:
-from_num = int(input("Enter the number to start from: "))
-
-next_palindrome_num = next_smallest_palindrome(from_num)
-
-print("Next smallest palindrome is:", next_palindrome_num)
